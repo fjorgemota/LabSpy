44314ddb09496f5c2e4b2e9b6f29e307009b3886
skyrios <epodesta158@gmail.com>
2015-04-12 17:02:04 -0300

    Update for grid

--- teacher/src/Main.java
+++ teacher/src/Main.java
@@ -3,10 +3,15 @@ import views.GridManager;
 /**
  * Created by fernando on 11/04/15.
  */
-
 public class Main {
     public static void main(String[] args) {
 		GridManager g = new GridManager(new ServerThread());
 		g.setVisible(true);
+		while(true) {
+			g.update();
+			try {
+				Thread.sleep(60);
+			} catch (Exception e) {}
+		}
 	}
 }
