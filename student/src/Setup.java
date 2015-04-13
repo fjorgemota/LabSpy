ce1c074264b864571a306b7fdb78a145311bf281
Fernando Paladini <fnpaladini@gmail.com>
2015-04-13 11:29:56 -0300

    Added new messages to the Setup.java

--- student/src/Setup.java
+++ student/src/Setup.java
@@ -8,15 +8,18 @@ import javax.swing.*;
 public class Setup {
     public static void main(String[] args) {
 
+        JOptionPane.showMessageDialog(null, "This setup will guide you installing LabSpy into student computers. You also need run this setup as an administrator (\"sudo\"). \nIf you already have installed LabSpy in this computer, previous configurations will be lost.");
+
         // TODO: Create a GUI to choose the address of teacher computer.
         String input = "";
         while(input.isEmpty()) {
             input = JOptionPane.showInputDialog("What's the hostname or IP from the teacher computer?");
         }
 
-
         Config config = Config.getInstance();
         config.setAddress(input);
+
+        JOptionPane.showMessageDialog(null, "The installation process was finished. Student computer should connect to the teacher's computer in the next reboot.");
 
     }
 }
