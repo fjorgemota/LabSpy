0fbaf2ee389acac34abdadf8ab128b734f52b19b
Fernando Paladini <fnpaladini@gmail.com>
2015-05-22 11:20:55 -0300

    ComputerManager v0.0.0.0 Beta finished (now professor is able to add computers and it's IPs by GUI).

--- common/config/Computer.java
+++ common/config/Computer.java
@@ -10,7 +10,7 @@ public class Computer implements Serializable {
 
     private String ip;
 
-    Computer(String ip) {
+    public Computer(String ip) {
         this.ip = ip;
     }
 
@@ -19,6 +19,10 @@ public class Computer implements Serializable {
     }
 
     public String getIp() {
+        return this.ip;
+    }
+    
+    public String toString() {
         return this.ip;
     }
 
