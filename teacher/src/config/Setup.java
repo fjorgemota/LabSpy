3831a355c7f8d693b0911200f67dc4c728ba9a8e
Fernando Paladini <fnpaladini@gmail.com>
2015-05-24 23:24:25 -0300

    Setup working with .jar files on startup (not tested).

--- teacher/src/config/Setup.java
+++ teacher/src/config/Setup.java
@@ -9,13 +9,13 @@ import java.io.IOException;
  * Esta classe é responsável por realizar o setup / instalação do Cliente LabSpy (máquinas dos alunos) em uma máquina.
  * Utiliza classes como Computer e RemoteSSH para tal.
  *
- * TODO: Currently this class expects clientSRC to be an executable file. As labspy_client will not be an executable (it's a JAR file), we must create a .sh script to put in /etc/init.d/.
  */
 public class Setup {
 
     private RemoteSSH ssh;
     private final String pathToInstall = "/var/lib/LabSpy/";
-    private final String clientSRC = "/home/paladini/meuProgramaQueVouEnviar";
+    private final String clientSRC = "out/artifacts/Student/Student.jar";
+    private final String scriptSRC = "assets/labspy.sh";
 
     public Setup(RemoteSSH ssh) {
         this.ssh = ssh;
@@ -37,13 +37,14 @@ public class Setup {
         // Transfering the Client for the remote machine.
         try {
             ssh.transferFile(clientSRC, pathToInstall);
+            ssh.transferFile(scriptSRC, pathToInstall);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         // Enabling the client to run at the startup.
         try {
-            ssh.executeCommand("sudo ln -s " + pathToInstall + clientSRC + " /etc/init.d/" + "labspy_client");
+            ssh.executeCommand("sudo ln -s /var/lib/LabSpy/labspy.sh /etc/init.d/labspy_client");
             ssh.executeCommand("sudo chmod +x /etc/init.d/labspy_client");
             ssh.executeCommand("sudo update-rc.d labspy_client defaults");
         } catch (IOException e) {
