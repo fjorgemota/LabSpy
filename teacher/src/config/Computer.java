40bbcfb12a626dc12f0fdc47e0a0120c744fd89d
Fernando Paladini <fnpaladini@gmail.com>
2015-06-12 22:46:53 -0300

    Added 'label' support for computer.

--- teacher/src/config/Computer.java
+++ teacher/src/config/Computer.java
@@ -8,18 +8,21 @@ import java.io.Serializable;
 public class Computer implements Serializable {
 
     private String ip;
+    private String label;
 
     public Computer(String ip) {
         this.ip = ip;
     }
+    public Computer(String ip, String label) { this.ip = ip; this.label = label;  }
 
     public void setIp(String ip) {
         this.ip = ip;
     }
-
     public String getIp() {
         return this.ip;
     }
+    public void setLabel(String label) { this.label = label; }
+    public String getLabel() { return this.label; }
     
     public String toString() {
         return this.ip;
