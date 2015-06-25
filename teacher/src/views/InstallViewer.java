cfe91fc061091acc5bae7b92105a0d379809d76d
Fernando Paladini <fnpaladini@gmail.com>
2015-06-25 01:15:16 -0300

    Solved SSH dependency problem. Thanks @fjorgemota !

--- teacher/src/views/InstallViewer.java
+++ teacher/src/views/InstallViewer.java
@@ -85,12 +85,12 @@ public class InstallViewer extends javax.swing.JFrame {
                     status_editable_label.setText("Installing and configuring " + c.toString() + "...");
 
                     // Doing the work
-                    try {
-                        //setup.install();
-                        Thread.sleep(1500);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(InstallViewer.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+//                    try {
+                        setup.install();
+//                        Thread.sleep(1500);
+//                    } catch (InterruptedException ex) {
+//                        Logger.getLogger(InstallViewer.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
 
                     // After installing Setup object.
                     progress_bar.setValue(i + 1);
