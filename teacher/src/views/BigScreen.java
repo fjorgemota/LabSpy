ad895990bd7cd99c961d162ec99c44c44cdfd4a7
Fernando Jorge Mota <f.j.mota13@gmail.com>
2015-06-27 19:05:42 -0300

    Allow BigScreen to have menubar :D

--- teacher/src/views/BigScreen.java
+++ teacher/src/views/BigScreen.java
@@ -11,7 +11,7 @@ import java.awt.event.*;
 /**
  * Created by fernando on 24/05/15.
  */
-public class BigScreen extends JFrame implements MouseListener, MouseWheelListener, MouseMotionListener, KeyListener, Runnable {
+public class BigScreen extends JFrame implements Runnable {
     private ClientThread client;
     private boolean stopped;
     private MouseMoveMessage position;
@@ -19,103 +19,17 @@ public class BigScreen extends JFrame implements MouseListener, MouseWheelListen
     static final String SEND = "SEND";
     static final String POWERDOWN = "POWERDOWN";
     static final String POWERUP = "POWERUP";
+    private BigScreenPanel panel;
 
     public BigScreen(ClientThread client, int fps) {
         super("LabSpy - BigScreen - "+client.getComputer().getIp());
         this.frames = fps;
         this.client = client;
-        this.addMouseListener(this);
-        this.addMouseMotionListener(this);
-        this.addMouseWheelListener(this);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.addKeyListener(this);
         this.setVisible(true);
         this.setSize(800, 600);
-
-
-
-    }
-    @Override
-    public void mouseClicked(MouseEvent e) {
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        int button;
-        switch(e.getButton()) {
-            default:
-            case MouseEvent.BUTTON1:
-                button = InputEvent.BUTTON1_MASK;
-                break;
-            case MouseEvent.BUTTON2:
-                button = InputEvent.BUTTON2_MASK;
-                break;
-            case MouseEvent.BUTTON3:
-                button = InputEvent.BUTTON3_MASK;
-                break;
-        }
-        this.client.sendMessage(new MousePressMessage(button));
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        int button;
-        switch(e.getButton()) {
-            default:
-            case MouseEvent.BUTTON1:
-                button = InputEvent.BUTTON1_MASK;
-                break;
-            case MouseEvent.BUTTON2:
-                button = InputEvent.BUTTON2_MASK;
-                break;
-            case MouseEvent.BUTTON3:
-                button = InputEvent.BUTTON3_MASK;
-                break;
-        }
-        this.client.sendMessage(new MouseReleaseMessage(button));
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
-        this.client.sendMessage(new MouseWheelMessage(e.getWheelRotation()));
-
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        this.client.sendMessage(new KeyPressMessage(e.getKeyCode()));
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        this.client.sendMessage(new KeyReleaseMessage(e.getKeyCode()));
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        this.position = new MouseMoveMessage(e.getX(), e.getY(), this.getWidth(), this.getHeight());
-
+        panel = new BigScreenPanel(client);
+        this.setContentPane(panel);
     }
 
     public void stop() {
@@ -134,13 +48,13 @@ public class BigScreen extends JFrame implements MouseListener, MouseWheelListen
 
         this.setSize(width, height);
         this.setIgnoreRepaint(false);
-        client.sendMessage(new ResizeScreenshot(new Rectangle(this.getWidth(), this.getHeight())));
+        client.sendMessage(new ResizeScreenshot(new Rectangle(width, height)));
         while (!this.stopped) {
             t = (t++) % this.frames;
-            if (t == 0 && (this.getWidth() != width || this.getHeight() != height)) {
-                client.sendMessage(new ResizeScreenshot(new Rectangle(this.getWidth(), this.getHeight())));
-                width = this.getWidth();
-                height = this.getHeight();
+            if (t == 0 && (panel.getWidth() != width || panel.getHeight() != height)) {
+                client.sendMessage(new ResizeScreenshot(new Rectangle(panel.getWidth(), panel.getHeight())));
+                width = panel.getWidth();
+                height = panel.getHeight();
             }
 
             if (!this.client.isRunning()) {
@@ -148,6 +62,10 @@ public class BigScreen extends JFrame implements MouseListener, MouseWheelListen
                 break;
             }
             if (this.isActive()) {
+                MouseMoveMessage p = panel.getPosition();
+                if (p != null) {
+                    this.position = p;
+                }
                 if (t == 0 && (this.position.getX() != x || this.position.getY() != y)) {
                     this.client.sendMessage(this.position);
                     x = this.position.getX();
@@ -171,15 +89,6 @@ public class BigScreen extends JFrame implements MouseListener, MouseWheelListen
     @Override
     public void dispose() {
         super.dispose();
-        this.stopped = true;
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        Image img = this.client.getLastScreenshot().getImage().getImage();
-        if (img == null) {
-            return;
-        }
-        g.drawImage(img, 0, 0, null);
+        this.stop();
     }
 }
