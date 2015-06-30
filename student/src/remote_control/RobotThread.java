45ef2f5a97ddef3f1c8f13915ce8ef8ee1b15f87
skyrios <epodesta158@gmail.com>
2015-06-30 11:31:27 -0300

    Fix Paladini's Cagada

--- student/src/remote_control/RobotThread.java
+++ student/src/remote_control/RobotThread.java
@@ -29,51 +29,53 @@ public class RobotThread implements Runnable {
     @Override
     public void run() {
         Robot r = null;
-        try {
-            if (r == null) {
-                r = new Robot();
-            }
-            RobotMessage msg;
-            msg = this.send.take();
-            synchronized (this) {
-                if (this.position != null) {
-                    int x = (int) ((
-                            screen.getWidth() / ((double) this.position.getWidth())
-                    ) * this.position.getX());
-                    int y = (int) ((
-                            screen.getHeight() / ((double) this.position.getHeight()))
-                            * this.position.getY());
-                    r.mouseMove(x, y);
-                    this.position = null;
-                }
-            }
-
+        while (true) {
             try {
-                if (msg instanceof KeyPressMessage) {
-                    r.keyPress(((KeyPressMessage) msg).getKeyCode());
-                } else if (msg instanceof KeyReleaseMessage) {
-                    r.keyRelease(((KeyReleaseMessage) msg).getKeyCode());
-                } else if (msg instanceof MousePressMessage) {
-                    MousePressMessage press = (MousePressMessage) msg;
-                    r.mousePress(press.getButton());
-                } else if (msg instanceof MouseReleaseMessage) {
-                    MouseReleaseMessage release = (MouseReleaseMessage) msg;
-                    r.mouseRelease(release.getButton());
-                } else if (msg instanceof MouseWheelMessage) {
-                    MouseWheelMessage wheel = (MouseWheelMessage) msg;
-                    r.mouseWheel(wheel.getWheel());
-                } else if (msg instanceof ScreenshotRequest) {
-                    ScreenshotRequest screenshotRequest = (ScreenshotRequest) msg;
-                    BufferedImage buf = r.createScreenCapture(screenshotRequest.getRect());
-                    screenshots.offer(new Screenshot(buf));
+                if (r == null) {
+                    r = new Robot();
                 }
-            } catch (IllegalArgumentException e2) {
-                e2.printStackTrace();
+                RobotMessage msg;
+                msg = this.send.take();
+                synchronized (this) {
+                    if (this.position != null) {
+                        int x = (int) ((
+                                screen.getWidth() / ((double) this.position.getWidth())
+                        ) * this.position.getX());
+                        int y = (int) ((
+                                screen.getHeight() / ((double) this.position.getHeight()))
+                                * this.position.getY());
+                        r.mouseMove(x, y);
+                        this.position = null;
+                    }
+                }
+
+                try {
+                    if (msg instanceof KeyPressMessage) {
+                        r.keyPress(((KeyPressMessage) msg).getKeyCode());
+                    } else if (msg instanceof KeyReleaseMessage) {
+                        r.keyRelease(((KeyReleaseMessage) msg).getKeyCode());
+                    } else if (msg instanceof MousePressMessage) {
+                        MousePressMessage press = (MousePressMessage) msg;
+                        r.mousePress(press.getButton());
+                    } else if (msg instanceof MouseReleaseMessage) {
+                        MouseReleaseMessage release = (MouseReleaseMessage) msg;
+                        r.mouseRelease(release.getButton());
+                    } else if (msg instanceof MouseWheelMessage) {
+                        MouseWheelMessage wheel = (MouseWheelMessage) msg;
+                        r.mouseWheel(wheel.getWheel());
+                    } else if (msg instanceof ScreenshotRequest) {
+                        ScreenshotRequest screenshotRequest = (ScreenshotRequest) msg;
+                        BufferedImage buf = r.createScreenCapture(screenshotRequest.getRect());
+                        screenshots.offer(new Screenshot(buf));
+                    }
+                } catch (IllegalArgumentException e2) {
+                    e2.printStackTrace();
+                }
+                r.waitForIdle();
+            } catch (AWTException e) {
+                e.printStackTrace();
+            } catch (InterruptedException ignored) {
             }
-            r.waitForIdle();
-        } catch (AWTException e) {
-            e.printStackTrace();
-        } catch (InterruptedException ignored) {
         }
     }
 
